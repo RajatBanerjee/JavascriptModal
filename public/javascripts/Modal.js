c22009a42f77dc69147c4bfa2020c6d7f90ea2aa
Rajat <rajatmig29@gmail.com>
2015-04-10 12:50:52 -0700

    2nd commit

--- public/javascripts/Modal.js
+++ public/javascripts/Modal.js
@@ -1,19 +1,15 @@
 (function () {
-       
+       //Global object
         this.JudyModal= {};
+
         var options = {},
-
-          
-
-            centerModal,
-
             closeModalEvent,
-
             defaultSettings = {
-                width: 'auto',
-                height: 'auto',
+                width: '250px',
+                height: '250px',
                 lock: false,
-                hideClose: false,
+                content:"<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Numquam nisi, temporibus quam iusto amet beatae ab nobis. Perferendis fugiat cupiditate quas iure alias odio suscipit voluptate facilis, dolorem sapiente adipisci.</p>",
+                closeButton: true,
                 draggable: false,
                 closeAfter: 0,
                 openCallback: false,
@@ -21,162 +17,77 @@
                 hideOverlay: false
             };
 
+        //variables required for building the modal
+        var modalOverlay,
+            modalContainer,
+            modalHeader,
+            modalContent,
+            modalClose;
+
+        /*
+        PUBLIC FUNCTIONS
+        */
+
         // Open the modal
         JudyModal.open = function (parameters) {
-            settings.width = parameters.width || defaultSettings.width;
-            settings.height = parameters.height || defaultSettings.height;
-            settings.lock = parameters.lock || defaultSettings.lock;
-            settings.hideClose = parameters.hideClose || defaultSettings.hideClose;
-            settings.draggable = parameters.draggable || defaultSettings.draggable;
-            settings.closeAfter = parameters.closeAfter || defaultSettings.closeAfter;
-            settings.closeCallback = parameters.closeCallback || defaultSettings.closeCallback;
-            settings.openCallback = parameters.openCallback || defaultSettings.openCallback;
-            settings.hideOverlay = parameters.hideOverlay || defaultSettings.hideOverlay;
-
-            centerModal = function () {
-                JudyModal.center({});
-            };
-
-            if (parameters.content && !parameters.ajaxContent) {
-                modalContent.innerHTML = parameters.content;
-            } else if (parameters.ajaxContent && !parameters.content) {
-                modalContainer.className = 'modal-loading';
-                JudyModal.ajax(parameters.ajaxContent, function insertAjaxResult(ajaxResult) {
-                    modalContent.innerHTML = ajaxResult;
-                });
-            } else {
-                modalContent.innerHTML = '';
-            }
-
-            modalContainer.style.width = settings.width;
-            modalContainer.style.height = settings.height;
-
-            JudyModal.center({});
-
-            if (settings.lock || settings.hideClose) {
-                modalClose.style.visibility = 'hidden';
-            }
-            if (!settings.hideOverlay) {
-                modalOverlay.style.visibility = 'visible';
-            }
-            modalContainer.style.visibility = 'visible';
+            
+                if(typeof(parameters)=="undefined"){
+                    options = defaultSettings;
+                }
+                else{
+                options.width = parameters.width || defaultSettings.width;
+                options.height = parameters.height || defaultSettings.height;
+                options.lock = parameters.lock || defaultSettings.lock;
+                options.closeButton = parameters.closeButton || defaultSettings.closeButton;
+                options.draggable = parameters.draggable || defaultSettings.draggable;
+                options.closeAfter = parameters.closeAfter || defaultSettings.closeAfter;
+                options.closeCallback = parameters.closeCallback || defaultSettings.closeCallback;
+                options.openCallback = parameters.openCallback || defaultSettings.openCallback;
+                options.hideOverlay = parameters.hideOverlay || defaultSettings.hideOverlay;
+                options.content= parameters.content || defaultSettings.content;
+                }
+            SetModalContainerOptions(options);
+            BuildModalInnerContent(options.content);
+            SentModalCenter();
+            setEventHandlers(options)
+      
 
             document.onkeypress = function (e) {
-                if (e.keyCode === 27 && settings.lock !== true) {
+                if (e.keyCode === 27 && options.lock !== true) {
                     JudyModal.close();
                 }
             };
 
-            modalClose.onclick = function () {
-                if (!settings.hideClose) {
-                    JudyModal.close();
-                } else {
-                    return false;
-                }
-            };
-            modalOverlay.onclick = function () {
-                if (!settings.lock) {
-                    JudyModal.close();
-                } else {
-                    return false;
-                }
-            };
+        
 
-            if (window.addEventListener) {
-                window.addEventListener('resize', centerModal, false);
-            } else if (window.attachEvent) {
-                window.attachEvent('onresize', centerModal);
-            }
+        // if (window.addEventListener) {
+        //     window.addEventListener('resize', centerModal, false);
+        // } else if (window.attachEvent) {
+        //     window.attachEvent('onresize', centerModal);
+        // }
 
-            if (settings.draggable) {
-                modalHeader.style.cursor = 'move';
-                modalHeader.onmousedown = function (e) {
-                    JudyModal.drag(e);
-                    return false;
-                };
-            } else {
-                modalHeader.onmousedown = function () {
-                    return false;
-                };
-            }
-            if (settings.closeAfter > 0) {
-                closeModalEvent = window.setTimeout(function () {
-                    JudyModal.close();
-                }, settings.closeAfter * 1000);
-            }
-            if (settings.openCallback) {
-                settings.openCallback();
-            }
+        // if (options.draggable) {
+        //     modalHeader.style.cursor = 'move';
+        //     modalHeader.onmousedown = function (e) {
+        //         JudyModal.drag(e);
+        //         return false;
+        //     };
+        // } else {
+        //     modalHeader.onmousedown = function () {
+        //         return false;
+        //     };
+        // }
+        // if (options.closeAfter > 0) {
+        //     closeModalEvent = window.setTimeout(function () {
+        //         JudyModal.close();
+        //     }, options.closeAfter * 1000);
+        // }
+        // if (options.openCallback) {
+        //     options.openCallback();
+        // }
         };
 
-        // Drag the modal
-        JudyModal.drag = function (e) {
-            var xPosition = (window.event !== undefined) ? window.event.clientX : e.clientX,
-                yPosition = (window.event !== undefined) ? window.event.clientY : e.clientY,
-                differenceX = xPosition - modalContainer.offsetLeft,
-                differenceY = yPosition - modalContainer.offsetTop;
-
-            document.onmousemove = function (e) {
-                xPosition = (window.event !== undefined) ? window.event.clientX : e.clientX;
-                yPosition = (window.event !== undefined) ? window.event.clientY : e.clientY;
-
-                modalContainer.style.left = ((xPosition - differenceX) > 0) ? (xPosition - differenceX) + 'px' : 0;
-                modalContainer.style.top = ((yPosition - differenceY) > 0) ? (yPosition - differenceY) + 'px' : 0;
-
-                document.onmouseup = function () {
-                    window.document.onmousemove = null;
-                };
-            };
-        };
-
-        // Perform XMLHTTPRequest
-        JudyModal.ajax = function (url, successCallback) {
-            var i,
-                XMLHttpRequestObject = false,
-                XMLHttpRequestObjects = [
-                    function () {
-                        return new window.XMLHttpRequest();  // IE7+, Firefox, Chrome, Opera, Safari
-                    },
-                    function () {
-                        return new window.ActiveXObject('Msxml2.XMLHTTP.6.0');
-                    },
-                    function () {
-                        return new window.ActiveXObject('Msxml2.XMLHTTP.3.0');
-                    },
-                    function () {
-                        return new window.ActiveXObject('Msxml2.XMLHTTP');
-                    }
-                ];
-
-            for (i = 0; i < XMLHttpRequestObjects.length; i += 1) {
-                try {
-                    XMLHttpRequestObject = XMLHttpRequestObjects[i]();
-                } catch (ignore) {
-                }
-
-                if (XMLHttpRequestObject !== false) {
-                    break;
-                }
-            }
-
-            XMLHttpRequestObject.open('GET', url, true);
-
-            XMLHttpRequestObject.onreadystatechange = function () {
-                if (XMLHttpRequestObject.readyState === 4) {
-                    if (XMLHttpRequestObject.status === 200) {
-                        successCallback(XMLHttpRequestObject.responseText);
-                        modalContainer.removeAttribute('class');
-                    } else {
-                        successCallback(XMLHttpRequestObject.responseText);
-                        modalContainer.removeAttribute('class');
-                    }
-                }
-            };
-
-            XMLHttpRequestObject.send(null);
-        };
-
-
+       
         // Close the modal
         JudyModal.close = function () {
             modalContent.innerHTML = '';
@@ -194,19 +105,69 @@
                 window.clearTimeout(closeModalEvent);
             }
 
-            if (settings.closeCallback) {
-                settings.closeCallback();
+            if (options.closeCallback) {
+                options.closeCallback();
             }
 
             if (window.removeEventListener) {
                 window.removeEventListener('resize', centerModal, false);
             } else if (window.detachEvent) {
-                window.detachEvent('onresize', centerModal);
+                window.detachEvent('onresize', SentModalCenter);
             }
         };
 
+        
+
+        //private functions
+        function BuildModalContent(){
+            modalOverlay = document.createElement('div'),
+            modalContainer = document.createElement('div'),
+            modalHeader = document.createElement('div'),
+            modalContent = document.createElement('div'),
+            modalClose = document.createElement('div');
+
+        
+
+            // Set the id's, append the nested elements, and append the complete modal to the document body
+            modalOverlay.setAttribute('id', 'modal-overlay');
+            modalContainer.setAttribute('id', 'modal-container');
+            modalHeader.setAttribute('id', 'modal-header');
+            modalContent.setAttribute('id', 'modal-content');
+            modalClose.setAttribute('id', 'modal-close');
+            modalClose.setAttribute('class','judy-modal-close');
+            modalHeader.appendChild(modalClose);
+            modalContainer.appendChild(modalHeader);
+            modalContainer.appendChild(modalContent);
+
+            modalClose.innerHTML  ="X"
+
+            modalOverlay.style.visibility = 'hidden';
+            modalContainer.style.visibility = 'hidden';
+
+             document.body.appendChild(modalOverlay);
+                document.body.appendChild(modalContainer);
+                 
+        }
+        
+        function SetModalContainerOptions (options){
+             modalContainer.style.width = options.width;
+            modalContainer.style.height = options.height;
+              if (options.lock || !options.closeButton) {
+            modalClose.style.visibility = 'hidden';
+            }
+            if (!options.hideOverlay) {
+                modalOverlay.style.visibility = 'visible';
+            }
+            modalContainer.style.visibility = 'visible';
+        }
+
+        function BuildModalInnerContent (innerContent) {
+             
+             modalContent.innerHTML = innerContent;  
+        }
+
         // Center the modal in the viewport
-        JudyModal.center = function (parameters) {
+        function SentModalCenter(parameters) {
             var documentHeight = Math.max(document.body.scrollHeight, document.documentElement.scrollHeight),
 
                 modalWidth = Math.max(modalContainer.clientWidth, modalContainer.offsetWidth),
@@ -237,40 +198,39 @@
                 amountScrolledX = document.documentElement.scrollLeft;
             }
 
-            if (!parameters.horizontalOnly) {
-                modalContainer.style.top = amountScrolledY + (browserHeight / 2) - (modalHeight / 2) + 'px';
-            }
-
+            
             modalContainer.style.left = amountScrolledX + (browserWidth / 2) - (modalWidth / 2) + 'px';
 
             modalOverlay.style.height = documentHeight + 'px';
             modalOverlay.style.width = '100%';
         };
-
-        //private functions
-        function BuildModal(){
-            var modalOverlay = document.createElement('div'),
-            modalContainer = document.createElement('div'),
-            modalHeader = document.createElement('div'),
-            modalContent = document.createElement('div'),
-            modalClose = document.createElement('div');
-
-            // Set the id's, append the nested elements, and append the complete modal to the document body
-            modalOverlay.setAttribute('id', 'modal-overlay');
-            modalContainer.setAttribute('id', 'modal-container');
-            modalHeader.setAttribute('id', 'modal-header');
-            modalContent.setAttribute('id', 'modal-content');
-            modalClose.setAttribute('id', 'modal-close');
-            modalHeader.appendChild(modalClose);
-            modalContainer.appendChild(modalHeader);
-            modalContainer.appendChild(modalContent);
-
-            modalOverlay.style.visibility = 'hidden';
-            modalContainer.style.visibility = 'hidden';
-
-             document.body.appendChild(modalOverlay);
-                document.body.appendChild(modalContainer);
+      
+             if (window.addEventListener) {
+            window.addEventListener('load', function () {
+                BuildModalContent();
+            }, false);
+        } else if (window.attachEvent) {
+            window.attachEvent('onload', function () {
+                BuildModalContent();
+            });
         }
         
-      
+        function setEventHandlers (parameters){
+
+            modalClose.addEventListener('click',function () {
+                if (parameters.closeButton) {
+                    JudyModal.close();
+                } else {
+                    return false;
+                }
+            }); 
+
+            modalOverlay.addEventListener('click', function () {
+                if (!parameters.lock) {
+                    JudyModal.close();
+                } else {
+                    return false;
+                }
+            });
+        }
     }());
--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,9 +1,3 @@
 function btnClick(){
-  JudyModal.open({
-    content: '<strong>Default modal!</strong><br />Testing the modal.<br /><br />Loreum ipsum dolorem the quick brown fox jumped over the lazy dog.<br /><br />Yes its true.',
-               draggable: true,
-               openCallback: function () {
-                   alert('This is an example of the use of openCallback');
-               }
-            });
+  JudyModal.open();
 }
